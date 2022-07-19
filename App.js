37ed49b9014fe37be7bc32ec2492d74dccfd2847
Julya <86087548+Ju-Vilela@users.noreply.github.com>
2022-07-19 20:52:52 -0300

    Pagina Principal

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+import Header from './Componentes/Header';
+import Footer from './Componentes/Footer';
+import HomePag from './Componentes/HomePag';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Header />
+      <HomePag />
+      <Footer />
+
+    </div>
+  );
+}
+
+export default App;
